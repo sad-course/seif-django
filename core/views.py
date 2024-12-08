4a8339998d4e2e0004397190ae49b0ba045b1301
Karen Dantas <karennobre74@gmail.com>
2024-12-08 17:53:27 -0300

    feat: finish details page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def details(request):
+    return render(request, 'core/event_detail.html')
