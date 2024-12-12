13d7fa05c1153fe90396928cfc0347f0fc658b14
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2024-12-12 15:47:20 -0300

    creation and configuration of my_certificates page

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('my_certificates/', views.my_certificates, name = 'my_certificates')
 ]
