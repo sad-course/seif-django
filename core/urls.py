ec44e2c806e7062fe8f960d8e9fa00d73c51a6a4
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2024-12-12 15:52:27 -0300

    bug correction in path for events and certificates page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('my_events/', views.my_certificates, name = 'my_events')
+    path('my_certificates/', views.my_certificates, name = 'my_certificates'),
+    path('my_events/', views.my_events, name = 'my_events')
 ]
