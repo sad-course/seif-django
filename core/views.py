f660130c68bf867ed8117bef96fa404c1e8d55d7
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2024-12-13 13:49:15 -0300

    bug correction in views for my events page

--- core/views.py
+++ core/views.py
@@ -7,5 +7,5 @@ def index(request):
 def my_certificates(request):
     return render(request, 'core/my_certificates.html')
 
-def my_certificates(request):
+def my_events(request):
     return render(request, 'core/my_events.html')
