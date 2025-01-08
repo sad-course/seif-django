f5553574b8c7fb88e71ea5aaf9efccf4aeaa576f
Karen Dantas <karennobre74@gmail.com>
2025-01-08 18:19:53 -0300

    feat: add responsitivy to management base-html

--- management/static/js/management_script.js
+++ management/static/js/management_script.js
@@ -1,17 +1,26 @@
-const ul = document.querySelectorAll('ul li');
+const ul = document.querySelectorAll('li');
 console.log(ul)
 
 //função temporária
 const url_atual = window.location.pathname;
 ul.forEach((item) => {
-    item.addEventListener('click', () => {
-        const selectedLink = item.querySelector(a);
-        console.log(item)
-        if (selectedLink.getAtribute('href') === url_atual) {
-            item.classList.add('aside-select-item')
-        }else{
-            console.log(selectedLink)
-        }
-    })
+    const selectedLink = item.querySelector('a');
+
+    if (selectedLink.getAttribute('href') === url_atual) {
+        item.classList.add('aside-select-item')
+    }else{
+        item.classList.remove('aside-select-item')
+    }
+
  
+})
+
+//menu hamburguer
+
+const hamburger = document.getElementById("hamburger-menu")
+const aside = document.querySelector('aside')
+
+
+hamburger.addEventListener( 'click', () => {
+    aside.classList.toggle("hidden");
 })
