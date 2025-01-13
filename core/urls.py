b4190313adf370faa2ebeaf1866e2e50c2ef3bb9
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2025-01-13 13:34:08 -0300

    templates of my certificates and my events

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('my_certificates/', views.my_certificates, name = 'my_certificates'),
-    path('my_events/', views.my_events, name = 'my_events')
+    path('my_events/', views.my_events, name = 'my_events'),
 ]
