b54822496722fc051b9ab95ca3093984bae1748b
Douglas-Lopesq <88059347+Douglas-Lopesq@users.noreply.github.com>
2025-01-14 17:00:13 -0300

    create page for 'create event'

--- management/urls.py
+++ management/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='management'), 
-    path('organizers/', views.organizers ,  name = 'organizers'),
-    path('participants/', views.participants , name = 'participants'),
+    path("", views.index, name="management"),
+    path("organizers/", views.organizers, name="organizers"),
+    path("participants/", views.participants, name="participants"),
+    path("create_event/", views.create_event, name="create_event"),
 ]
