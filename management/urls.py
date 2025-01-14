98cb71148f3859ca6cf054a777e739133e03b2ea
Mirla Lopes <mirlaisabel11@gmail.com>
2025-01-14 20:44:37 -0300

    feat: Create templates and view functions for event submit requests

--- management/urls.py
+++ management/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path("", views.index, name="management"),
     path("organizers/", views.organizers, name="organizers"),
     path("participants/", views.participants, name="participants"),
+    path("analytics/home/", views.analytics_home, name="analytics_home"),
+    path(
+        "analytics/event/", views.analytics_event_detail, name="analytics_event_detail"
+    ),
+    path("event/request/", views.event_publish_request, name="event_publish_request"),
 ]
--- management/views.py
+++ management/views.py
@@ -3,10 +3,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'management/index.html') 
+    return render(request, "management/index.html")
+
 
 def organizers(request):
-    return render(request, 'management/organizers.html')
+    return render(request, "management/organizers.html")
+
 
 def participants(request):
-    return render(request, 'management/participants.html')
+    return render(request, "management/participants.html")
+
+
+def analytics_home(request):
+    return render(request, "management/analytics_home.html")
+
+
+def analytics_event_detail(request):
+    return render(request, "management/analytics_event_detail.html")
+
+
+def event_publish_request(request):
+    return render(request, "management/organizer_event_submit_requests.html")
