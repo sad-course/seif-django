f7765b799b6e072d72528e86e06fb1ee824faebc
Mirla Lopes <mirlaisabel11@gmail.com>
2025-01-14 00:07:15 -0300

    feat: Add reset password urls paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import login, signup, profile
+from .views import login, signup, profile, reset_password, request_reset_password
 
 urlpatterns = [
-    path('login', login, name='login'),
-    path('signup', signup, name='signup'),
-    path('profile', profile, name='profile'),
-
+    path("login/", login, name="login"),
+    path("signup/", signup, name="signup"),
+    path("profile/", profile, name="profile"),
+    path(
+        "request_reset_password/", request_reset_password, name="request_reset_password"
+    ),
+    path("reset_password/", reset_password, name="reset_password"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    return render(request, "accounts/profile.html")
+
 
 def login(request):
     context = {}
-    return render(request, 'accounts/login.html', context=context)
+    return render(request, "accounts/login.html", context=context)
+
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    return render(request, "accounts/signup.html")
+
+
+def request_reset_password(request):
+    return render(request, "accounts/request_reset_password.html")
+
+
+def reset_password(request):
+    return render(request, "accounts/reset_password.html")
