8fd5d032df738ad0b6b317841f6a73f5b2a20948
Karen Dantas <karennobre74@gmail.com>
2025-02-07 16:04:02 -0300

    feat: add class based views to management app

--- management/urls.py
+++ management/urls.py
@@ -2,23 +2,39 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="management"),
-    path("organizers/", views.organizers, name="organizers"),
-    path("participants/", views.participants, name="participants"),
-    path("create_event/", views.create_event, name="create_event"),
+    path("", views.Index.as_view(), name="management"),
+    path("organizers/", views.Organizers.as_view(), name="organizers"),
+    path("participants/", views.Participants.as_view(), name="participants"),
+    path("create_event/", views.CreateEvent.as_view(), name="create_event"),
     path(
-        "request_create_event/", views.request_create_event, name="request_create_event"
+        "request_create_event/",
+        views.RequestCreateEvent.as_view(),
+        name="request_create_event",
     ),
-    path("analytics/home/", views.analytics_home, name="analytics_home"),
+    path("analytics/home/", views.AnalyticsHome.as_view(), name="analytics_home"),
     path(
-        "analytics/event/", views.analytics_event_detail, name="analytics_event_detail"
+        "analytics/event/",
+        views.AnalyticsEventDetail.as_view(),
+        name="analytics_event_detail",
     ),
-    path("event/request/", views.event_publish_requests, name="event_publish_request"),
+    path(
+        "event/request/",
+        views.EventPublishRequests.as_view(),
+        name="event_publish_request",
+    ),
     path(
         "event/request/detail/",
-        views.event_publish_request_detail,
+        views.EventPublishRequestDetail.as_view(),
         name="event_request_detail",
     ),
-    path("event/submit/", views.event_submit_dashboard, name="event_submit_dashboard"),
-    path("event/submit/detail/", views.event_submit_detail, name="event_submit_detail"),
+    path(
+        "event/submit/",
+        views.EventSubmitDashboard.as_view(),
+        name="event_submit_dashboard",
+    ),
+    path(
+        "event/submit/detail/",
+        views.EventSubmitDetail.as_view(),
+        name="event_submit_detail",
+    ),
 ]
--- management/views.py
+++ management/views.py
@@ -1,46 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def index(request):
-    return render(request, "management/index.html")
+class Index(TemplateView):
+    template_name = "management/index.html"
 
 
-def organizers(request):
-    return render(request, "management/organizers.html")
+class Organizers(TemplateView):
+    template_name = "management/organizers.html"
 
 
-def participants(request):
-    return render(request, "management/participants.html")
+class Participants(TemplateView):
+    template_name = "management/participants.html"
 
 
-def analytics_home(request):
-    return render(request, "management/analytics_home.html")
+class AnalyticsHome(TemplateView):
+    template_name = "management/analytics_home.html"
 
 
-def analytics_event_detail(request):
-    return render(request, "management/analytics_event_detail.html")
-  
-
-def create_event(request):
-    return render(request, "management/create_event.html")
-
-  
-def request_create_event(request):
-    return render(request, "management/request_create_event.html")
-  
-  
-def event_publish_requests(request):
-    return render(request, "management/organizer_event_submit_requests.html")
+class AnalyticsEventDetail(TemplateView):
+    template_name = "management/analytics_event_detail.html"
 
 
-def event_publish_request_detail(request):
-    return render(request, "management/organizer_event_submit_detail.html")
+class CreateEvent(TemplateView):
+    template_name = "management/create_event.html"
 
 
-def event_submit_dashboard(request):
-    return render(request, "management/event_submit_dashboard.html")
+class RequestCreateEvent(TemplateView):
+    template_name = "management/request_create_event.html"
 
 
-def event_submit_detail(request):
-    return render(request, "management/event_submit_detail.html")
+class EventPublishRequests(TemplateView):
+    template_name = "management/organizer_event_submit_requests.html"
+
+
+class EventPublishRequestDetail(TemplateView):
+    template_name = "management/organizer_event_submit_detail.html"
+
+
+class EventSubmitDashboard(TemplateView):
+    template_name = "management/event_submit_dashboard.html"
+
+
+class EventSubmitDetail(TemplateView):
+    template_name = "management/event_submit_detail.html"
