280c56bce0555adceead7b873683219eb564314b
Mirla Lopes <mirlaisabel11@gmail.com>
2025-02-14 07:00:20 -0300

    feat: Config python-social-core and SuapOAuth backends; Set login path to suap; Add LOGOUT_REDIRECT_URL to `home`

--- SEIF/settings.py
+++ SEIF/settings.py
@@ -38,8 +38,9 @@ if env_path is None:
 
 dotenv.load_dotenv(env_path)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+SOCIAL_AUTH_SUAP_KEY = os.getenv("SOCIAL_AUTH_SUAP_KEY")
+SOCIAL_AUTH_SUAP_SECRET = os.getenv("SOCIAL_AUTH_SUAP_SECRET")
+
 
 NPM_BIN_PATH = which("npm")
 
@@ -69,6 +70,7 @@ INSTALLED_APPS = [
     "django_browser_reload",
     "dj_svg",
     "django_ckeditor_5",
+    "social_django",
     # local apps
     "core",
     "accounts",
@@ -89,6 +91,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    # Social Django
+    "social_django.middleware.SocialAuthExceptionMiddleware",
 ]
 
 ROOT_URLCONF = "SEIF.urls"
@@ -106,6 +110,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # Social Django
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
@@ -182,11 +189,23 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom admin user
 AUTH_USER_MODEL = "accounts.Participant"
+LOGOUT_REDIRECT_URL = "home"
 
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
 
 AUTHENTICATION_BACKENDS = (
-    # 'authentication.backends.suap.SuapOAuth2',
     "django.contrib.auth.backends.ModelBackend",
+    "suap.backends.SuapOAuth2",
 )
 
 sentry_sdk.init(
