7928f1f4a29fac268703087c6237c34b4cdb0ed4
Karen Dantas <karennobre74@gmail.com>
2025-02-18 15:18:14 -0300

    feat: create participants listing in management, and table pagination

--- management/views.py
+++ management/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Participant
 
 
 # Create your views here.
@@ -10,8 +12,16 @@ def organizers(request):
     return render(request, "management/organizers.html")
 
 
-def participants(request):
-    return render(request, "management/participants.html")
+class Participants(ListView):
+    model = Participant
+    template_name = "management/participants.html"
+    context_object_name = "participants"
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["participants_count"] = Participant.objects.all().count()
+        return context
 
 
 def analytics_home(request):
@@ -20,16 +30,16 @@ def analytics_home(request):
 
 def analytics_event_detail(request):
     return render(request, "management/analytics_event_detail.html")
-  
+
 
 def create_event(request):
     return render(request, "management/create_event.html")
 
-  
+
 def request_create_event(request):
     return render(request, "management/request_create_event.html")
-  
-  
+
+
 def event_publish_requests(request):
     return render(request, "management/organizer_event_submit_requests.html")
 
