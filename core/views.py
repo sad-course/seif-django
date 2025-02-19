0bcaeb1dbd5048673237f7ac1f20f8b87d17e341
Douglas-Lopesq <douglaslopesq@gmail.com>
2025-02-19 08:27:37 -0300

    fix: number pagination

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Event
 from .filters import EventFilter
 
@@ -22,10 +23,39 @@ class Index(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        paginator = Paginator(queryset, self.paginate_by)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
 
-        context["filter"] = EventFilter(self.request.GET, queryset=self.get_queryset())
+        context["filter"] = EventFilter(self.request.GET, queryset=queryset)
         context["status_choices"] = filtered_choices
         context["campus_choices"] = Event.Campus.choices
+        context["page_obj"] = page_obj
+
+        # Lógica para exibir até 3 páginas consecutivas corretamente:
+        num_pages = paginator.num_pages
+        current_page = page_obj.number
+
+        if num_pages <= 3:
+            page_range = list(range(1, num_pages + 1))
+        else:
+            start_page = max(1, current_page - 1)
+            end_page = min(num_pages, current_page + 1)
+            page_range = list(range(start_page, end_page + 1))
+
+            # Adiciona reticências se necessário
+            if start_page > 1:
+                page_range.insert(0, 1)
+                if start_page > 2:
+                    page_range.insert(1, None)  # Reticências
+
+            if end_page < num_pages:
+                if end_page < num_pages - 1:
+                    page_range.append(None)  # Reticências
+                page_range.append(num_pages)
+
+        context["page_range"] = page_range
         return context
 
 
